fb90931a85add0ee217d6c2747797b12c241affd
Kristina <ya.kep0602@yandex.ru>
2022-08-04 00:53:04 +0300

    приветствие

---
+++ example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите имя пользователя: "); 
+string username = Console.ReadLine (); 
+
+if (username.ToLower() == "tom")
+{
+Console.WriteLine ("Салют!");
+    }
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+
+}
